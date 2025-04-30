8cb5f6264168f20cc116466bf3970d5c1d60814f
linuxnoob235 <tedraber09@gmail.com>
2025-04-30 13:09:36 -0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,6 @@ set -Ux fish_user_paths $fish_user_paths /home/efe/.scripts
 export GRIMSHOT_DIR="$HOME/Pictures/Screenshots"
 fastfetch
 # ~/.config/fish/config.fish
-set -gx TERM foot
+set -gx TERM alacritty
 set -gx COLORTERM truecolor
 end
