2239d63d9202635fceadb5c4a1f27b512795f33d
linuxnoob235 <tedraber09@gmail.com>
2025-04-30 13:08:57 -0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-set -Ux TERMINAL foot
+set -Ux TERMINAL alacritty
 
 set -Ux fish_user_paths $fish_user_paths /home/efe/.scripts
 export GRIMSHOT_DIR="$HOME/Pictures/Screenshots"
